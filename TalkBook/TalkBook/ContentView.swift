e51c2edcd07de131f8704d7900e9ebd34dcbaf31
Imran <imran.mia@portonics.com>
2023-10-21 23:32:29 +0600

    init

---
+++ TalkBook/TalkBook/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TalkBook
+//
+//  Created by Imran on 21/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TalkBook/TalkBook/TalkBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  TalkBookApp.swift
+//  TalkBook
+//
+//  Created by Imran on 21/10/23.
+//
+
+import SwiftUI
+
+@main
+struct TalkBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
