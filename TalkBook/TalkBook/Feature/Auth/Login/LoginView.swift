a15ce092ae6b1dbdf8a4b245f396f36166b816c0
Imran <imran.mia@portonics.com>
2023-10-27 22:11:00 +0600

    navigation flow with signin and signup

--- TalkBook/TalkBook/Feature/Auth/Login/LoginView.swift
+++ TalkBook/TalkBook/Feature/Auth/Login/LoginView.swift
@@ -19,39 +19,51 @@ struct LoginView: View {
     @State private var borderColor:Color? = Color.gray
     
     @State private var isShwoModal = false
+    @State private var route: Bool = false
     
     init(){
         print(loginVM.isSuccess)
     }
     var body: some View {
-        ZStack{
-            Color.black
-                .ignoresSafeArea()
-            VStack(spacing: 16){
-                loginView
+        NavigationStack{
+            ZStack{
+                Color.black
+                    .ignoresSafeArea()
+                VStack(spacing: 16){
+                    loginView
+                    
+                    Button("Login", action: {
+                        isShwoModal =  loginVM.isSuccess
+                        print("isShwoModal: \(isShwoModal)")
+                        //registraionVM.isSuccess = false
+                        loginVM.loginApiCall(username: username, password: password)
+                    })
+                    .buttonStyle(KitBaseButtonStyle(size: .lg, variant: .outline, backgroundColor: .clear, borderColor: .gray, foregroundColor: .white, buttonWidth: UIScreen.main.bounds.width * 0.8, borderWidth: 1))
+                    
+                    
+                    Text("-------------OR-------------")
+                        .font(.caption)
+                        .foregroundColor(.white)
+                    
+                    Button("Signup", action: {
+                        self.route = true
+                        print("Signup")
+                    })
+                    .buttonStyle(KitBaseButtonStyle(size: .lg, variant: .solid, backgroundColor: .red, borderColor: .clear, foregroundColor: .white, buttonWidth: UIScreen.main.bounds.width * 0.8, borderWidth: 1))
+                    
+                    Text(loginVM.isSuccess == false ? loginVM.user?.message ?? "" : "")
+                        .foregroundColor(.red)
+                }
+                .padding(.horizontal, 16)
                 
-                Button("Login", action: {
-                    isShwoModal =  loginVM.isSuccess
-                    print("isShwoModal: \(isShwoModal)")
-                    //registraionVM.isSuccess = false
-                    loginVM.loginApiCall(username: username, password: password)
-                })
-                .buttonStyle(KitBaseButtonStyle(size: .lg, variant: .outline, backgroundColor: .clear, borderColor: .gray, foregroundColor: .white, buttonWidth: UIScreen.main.bounds.width * 0.8, borderWidth: 1))
-                                
-                Text(loginVM.isSuccess == false ? loginVM.user?.message ?? "" : "")
-                    .foregroundColor(.red)
+                if loginVM.isSuccess{
+                    LoadingView(progressColor: .red)
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                }
             }
-            .padding(.horizontal, 16)
-            
-            if loginVM.isSuccess{
-                LoadingView(progressColor: .red)
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
-            }
-            
-//            if registraionVM.isSuccess {
-//                showPopup()
-//            }
-            
+            .navigationDestination(isPresented: $route, destination: {
+                RegistrationView()
+            })
         }
     }
     
