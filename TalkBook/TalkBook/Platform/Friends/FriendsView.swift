3a1a13ed4b9e7a65e2988ba01e719a6c1a7b244f
Imran <imran.mia@portonics.com>
2023-10-31 20:57:08 +0600

    tabsetup

---
+++ TalkBook/TalkBook/Platform/Friends/FriendsView.swift
@@ -0,0 +1,18 @@
+//
+//  FriendsView.swift
+//  TalkBook
+//
+//  Created by Imran on 31/10/23.
+//
+
+import SwiftUI
+
+struct FriendsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FriendsView()
+}
---
+++ TalkBook/TalkBook/Platform/Notifications/NotificationsView.swift
@@ -0,0 +1,18 @@
+//
+//  NotificationsView.swift
+//  TalkBook
+//
+//  Created by Imran on 31/10/23.
+//
+
+import SwiftUI
+
+struct NotificationsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NotificationsView()
+}
